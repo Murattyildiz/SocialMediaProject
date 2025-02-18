c926a89deb0a6edee25da5621d3a7554d9a79e36
muratyildizzz <yildizmurat42953@gmail.com>
2025-02-19 01:35:19 +0300

    Login teması eklendi.AuthController oluşturuldu.

---
+++ SosyalMedya_Web/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SosyalMedya_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet("giris-yap")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
