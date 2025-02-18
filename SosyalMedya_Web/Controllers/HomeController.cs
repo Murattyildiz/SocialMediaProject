bb10a8abe53497a87aef4a13b081758abb4df7d7
muratyildizzz <yildizmurat42953@gmail.com>
2025-02-18 17:33:22 +0300

    Frontend kısmı eklendi.Ana sayfa oluşturuldu.

---
+++ SosyalMedya_Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using SosyalMedya_Web.Models;
+using System.Diagnostics;
+
+namespace SosyalMedya_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+       public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
