a4952e0936ba36677f6de32213cef501df0ddae6
muratyildizzz <yildizmurat42953@gmail.com>
2025-02-21 01:11:43 +0300

    Konu başlıkları liste halinde gösterildi.

---
+++ SosyalMedya_Web/Models/Topics.cs
@@ -0,0 +1,9 @@
+﻿namespace SosyalMedya_Web.Models
+{
+    public class Topics
+    {
+        public int Id { get; set; }
+        public string TopicTitle { get; set; }
+        public bool Status { get; set; }
+    }
+}
--- SosyalMedya_Web/ViewCompanents/FormTopic/_FormTopic.cs
+++ SosyalMedya_Web/ViewCompanents/FormTopic/_FormTopic.cs
@@ -1,7 +1,30 @@
-﻿namespace SosyalMedya_Web.ViewCompanents.FormTopic
-{
-    public class _FormTopic
-    {
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SosyalMedya_Web.Models;
 
+namespace SosyalMedya_Web.ViewComponents.FormTopic
+{
+    public class _FormTopic : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FormTopic(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var responseMessage = await _httpClientFactory.CreateClient().GetAsync("https://localhost:5190/api/Topics/getall");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+
+                var apiDataResponse = JsonConvert.DeserializeObject<ApiListDataResponse<Topics>>(jsonResponse);
+                var trueTopic = apiDataResponse.Data.Where(x => x.Status == true).ToList();
+                return apiDataResponse.Success ? View(trueTopic) : View();
+            }
+            return View();
+        }
     }
 }
